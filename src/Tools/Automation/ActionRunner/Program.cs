d175cddaf476f7d812afba14074343b95d060953
Youssef1313 <youssefvictor00@gmail.com>
2021-08-17 18:23:26 +0200

    initial commit

---
+++ src/Tools/Automation/ActionRunner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Automation.ActionRunner
+{
+	class Program
+    {
+		private static void Main()
+        {
+			if (!Directory.Exists("microsoft-ui-xaml/dev"))
+			{
+				throw new Exception("Can't find microsoft-ui-xaml/dev directory.")
+			}
+        }
+    }
+}
