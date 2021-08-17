ed82f1b7bfc0a5da2769b4e3d2b9ecf68b02f4e1
Youssef1313 <youssefvictor00@gmail.com>
2021-08-17 19:11:23 +0200

    missing file

---
+++ src/Tools/Automation/ActionRunner/GitHubLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionRunner
+{
+	internal static class GitHubLogger
+	{
+		public static void LogInformation(string text) => Console.WriteLine(text);
+
+		public static void LogError(string text) => Console.WriteLine($"::error::{text}");
+	}
+}
