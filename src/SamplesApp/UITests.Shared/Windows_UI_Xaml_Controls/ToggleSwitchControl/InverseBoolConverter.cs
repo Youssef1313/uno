7f7a6e1a54d74e2e1774f039947aabaf7e2598f0
Youssef1313 <youssefvictor00@gmail.com>
2022-09-29 12:10:29 +0200

    test: Add test for issue 10006

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ToggleSwitchControl/InverseBoolConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace UITests.Windows_UI_Xaml_Controls.ToggleSwitchControl
+{
+    class InverseBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language) => !(bool)value;
+
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language) => !(bool)value;
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/ToggleSwitchControl/WrappedToggleSwitch.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Windows_UI_Xaml_Controls.ToggleSwitchControl
+{
+	public sealed partial class WrappedToggleSwitch : UserControl
+    {
+		public static readonly DependencyProperty SecondaryIsDisabledProperty =
+			DependencyProperty.Register(
+			nameof(SecondaryIsDisabled),
+			typeof(bool),
+			typeof(WrappedToggleSwitch),
+			new PropertyMetadata(false));
+
+		public bool SecondaryIsDisabled
+		{
+			get => (bool)GetValue(SecondaryIsDisabledProperty);
+			set => SetValue(SecondaryIsDisabledProperty, value);
+		}
+
+		public WrappedToggleSwitch()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
