8a5fa6142c7424bcfba4c8a3c785fa006bdb5adc
Youssef Victor <youssefvictor00@gmail.com>
2023-11-22 08:35:09 +0200

    For debugging in uno

--- src/SamplesApp/SamplesApp.Shared/MainPage.xaml.cs
+++ src/SamplesApp/SamplesApp.Shared/MainPage.xaml.cs
@@ -1,17 +1,41 @@
-﻿using SampleControl.Presentation;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
+﻿using System.Linq;
+using Microsoft.UI.Xaml.Controls;
 
 namespace SamplesApp
 {
-	public sealed partial class MainPage : Page
+
+	public sealed partial class MainPage : Windows.UI.Xaml.Controls.Page
 	{
+
+		private int count = 0;
+
 		public MainPage()
 		{
 			this.InitializeComponent();
 
-			sampleControl.DataContext = new SampleChooserViewModel(sampleControl);
+			this.navigationView.ItemInvoked += this.NavigationView_ItemInvoked;
 		}
+
+		private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+		{
+			NavigationViewItem saleItem = new NavigationViewItem() { IsSelected = true };
+
+			this.navigationView.MenuItems.Add(saleItem);
+			this.navigationView.SelectedItem = saleItem;
+			if (++count == 2)
+			{
+				PageClose(this.navigationView.MenuItems.Last() as NavigationViewItem);
+				PageClose(this.navigationView.MenuItems.Last() as NavigationViewItem);
+			}
+		}
+
+		private void PageClose(NavigationViewItem item)
+		{
+			this.navigationView.MenuItems.Remove(item);
+			this.navigationView.SelectedItem = this.navigationView.MenuItems.Last();
+		}
+
+
 	}
+
 }
